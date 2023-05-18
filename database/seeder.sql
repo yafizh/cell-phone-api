6640580b0710d6a6368a4328298d8770d24c6b8e
yafizh <a.chairil.yafizh@gmail.com>
2023-05-18 13:40:02 +0700

    trigger functions

--- database/seeder.sql
+++ database/seeder.sql
@@ -1,3 +1,17 @@
+INSERT INTO `db_cell_phone`.`users` (
+    `username`,
+    `password`,
+    `status` 
+) VALUES 
+('admin', 'admin', 'ADMIN'),
+('sahrul', 'sahrul', 'EMPLOYEE');
+
+INSERT INTO `db_cell_phone`.`employees` (
+    `user_id`,
+    `name` 
+) VALUES 
+(1, 'sahrul');
+
 INSERT INTO `db_cell_phone`.`item_types` (
     `name`,
     `order` 
@@ -5,3 +19,17 @@ INSERT INTO `db_cell_phone`.`item_types` (
 ('Aksesoris', 1),
 ('Casing HP', 2),
 ('Batrai HP', 3);
+
+INSERT INTO `db_cell_phone`.`item_sales` (
+    `employee_id`,
+    `sold_at`
+) VALUES 
+(1, CURRENT_TIMESTAMP);
+
+INSERT INTO `db_cell_phone`.`item_sale_details` (
+    `item_sale_id`,
+    `item_id`,
+    `count`,
+    `price`
+) VALUES 
+(1, 2, 5, 50000);
--- database/trigger.sql
+++ database/trigger.sql
@@ -1,44 +1,93 @@
+DELIMITER $$
+
 CREATE TRIGGER 
-	after_insert_supply_stock
+	after_insert_item_sale_details
 AFTER INSERT 
 ON 
-	supply_stock 
+	item_sale_details 
 FOR EACH ROW 
 BEGIN
 	UPDATE 
-		items 
+		items
 	SET 
-		stock = stock + NEW.stock 
+		stock = stock - NEW.count 
 	WHERE 
-		id = NEW.item_id 
-END
+		id = NEW.item_id;
+END $$
 
 CREATE TRIGGER 
-	after_update_supply_stock
+	after_update_item_sale_details
 AFTER UPDATE
 ON 
-	supply_stock 
+	item_sale_details 
 FOR EACH ROW 
 BEGIN
 	UPDATE 
-		items 
+		items
 	SET 
-		stock = stock - OLD.stock + NEW.stock 
+		stock = (stock + OLD.count) - NEW.count 
 	WHERE 
-		id = NEW.item_id 
-END
+		id = NEW.item_id;
+END $$
 
 CREATE TRIGGER 
-	after_delete_supply_stock
-AFTER DELETE
+	after_delete_item_sale_details
+AFTER DELETE 
 ON 
-	supply_stock 
+	item_sale_details 
 FOR EACH ROW 
 BEGIN
 	UPDATE 
-		items 
+		items
 	SET 
-		stock = stock - OLD.stock
+		stock = stock + OLD.count 
 	WHERE 
-		id = NEW.item_id 
-END
+		id = OLD.item_id;
+END $$
+
+-- 
+
+CREATE TRIGGER 
+	after_insert_item_supply_details
+AFTER INSERT 
+ON 
+	item_supply_details 
+FOR EACH ROW 
+BEGIN
+	UPDATE 
+		items
+	SET 
+		stock = stock + NEW.count 
+	WHERE 
+		id = NEW.item_id;
+END $$
+
+CREATE TRIGGER 
+	after_update_item_supply_details
+AFTER UPDATE 
+ON 
+	item_supply_details 
+FOR EACH ROW 
+BEGIN
+	UPDATE 
+		items
+	SET 
+		stock = (stock - OLD.count) + NEW.count 
+	WHERE 
+		id = NEW.item_id;
+END $$
+
+CREATE TRIGGER 
+	after_delete_item_supply_details
+AFTER DELETE 
+ON 
+	item_supply_details 
+FOR EACH ROW 
+BEGIN
+	UPDATE 
+		items
+	SET 
+		stock = stock - OLD.count 
+	WHERE 
+		id = OLD.item_id;
+END $$
