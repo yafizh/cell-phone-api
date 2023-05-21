8fa62bea6679f000ddf4dbae1596b9f8dc7abf29
yafizh <a.chairil.yafizh@gmail.com>
2023-05-21 21:12:04 +0700

    adjusment api for paging data

--- api/item_types/index.php
+++ api/item_types/index.php
@@ -1,7 +1,39 @@
 <?php
 
-$stmt = $conn->prepare("SELECT * FROM item_types ORDER BY `order`");
+$query = "
+    SELECT 
+        * 
+    FROM 
+        item_types 
+    ORDER BY 
+        `order` 
+";
+
+if (isset($_GET['page']) && isset($_GET['limit'])) {
+    $limit = $_GET['limit'];
+    $offset = ($_GET['page'] - 1) * $limit;
+
+    $query .= " LIMIT {$limit} OFFSET {$offset}";
+
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    $items = $stmt->fetchAll();
+
+    $stmt = $conn->prepare("SELECT COUNT(*) FROM item_types");
+    $stmt->execute();
+    $itemsLength = $stmt->fetchColumn();
+
+    echo json_encode([
+        'items' => $items,
+        'itemsLength' => $itemsLength
+    ]);
+    exit;
+}
+
+$stmt = $conn->prepare($query);
 $stmt->execute();
 $stmt->setFetchMode(PDO::FETCH_ASSOC);
-
 echo json_encode($stmt->fetchAll());
+exit;
