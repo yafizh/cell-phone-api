08c1eaf10c9c661f6cabb04cc72f28174cc8335a
yafizh <a.chairil.yafizh@gmail.com>
2023-05-22 07:51:28 +0700

    feature paging (limit, offset) endpoint items, item in and item out

--- api/item_out/index.php
+++ api/item_out/index.php
@@ -22,6 +22,30 @@ $query = "
     ORDER BY 
         io.out_at DESC 
 ";
+
+if (isset($_GET['page']) && isset($_GET['limit'])) {
+    $limit = $_GET['limit'];
+    $offset = ($_GET['page'] - 1) * $limit;
+
+    $query .= " LIMIT {$limit} OFFSET {$offset}";
+
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    $items = $stmt->fetchAll();
+
+    $stmt = $conn->prepare("SELECT COUNT(*) FROM item_out");
+    $stmt->execute();
+    $itemsLength = $stmt->fetchColumn();
+
+    echo json_encode([
+        'items' => $items,
+        'itemsLength' => $itemsLength
+    ]);
+    exit;
+}
+
 $stmt = $conn->prepare($query);
 $stmt->execute();
 $stmt->setFetchMode(PDO::FETCH_ASSOC);
--- api/items/index.php
+++ api/items/index.php
@@ -13,9 +13,36 @@ $query = "
     WHERE 
         i.item_type_id=:id 
 ";
+
+if (isset($_GET['page']) && isset($_GET['limit'])) {
+    $limit = $_GET['limit'];
+    $offset = ($_GET['page'] - 1) * $limit;
+
+    $query .= " LIMIT {$limit} OFFSET {$offset}";
+
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':id', $_GET['item_type_id']);
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    $items = $stmt->fetchAll();
+
+    $stmt = $conn->prepare("SELECT COUNT(*) FROM items WHERE item_type_id=:id");
+    $stmt->bindParam(':id', $_GET['item_type_id']);
+    $stmt->execute();
+    $itemsLength = $stmt->fetchColumn();
+
+    echo json_encode([
+        'items' => $items,
+        'itemsLength' => $itemsLength
+    ]);
+    exit;
+}
+
 $stmt = $conn->prepare($query);
 $stmt->bindParam(':id', $_GET['item_type_id']);
 $stmt->execute();
 $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
 echo json_encode($stmt->fetchAll());
+exit;
